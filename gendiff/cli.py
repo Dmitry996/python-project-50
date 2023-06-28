4b0e9fca80a6e51b975c97f52bfc31e2349d0a51
Dmitry996 <dimoon-96@yandex.ru>
2023-06-28 19:08:01 +0700

    edit cli.py

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -6,4 +6,5 @@ def gendiff():
         description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', "--format", help='set format of output')
     return parser.parse_args()
