07650bdd3523722b9c704198279e00c6ee973918
Dmitry996 <dimoon-96@yandex.ru>
2023-06-29 18:04:54 +0700

    add generate_diff

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,4 +1,6 @@
 import argparse
+import json
+import json
 
 
 def gendiff():
@@ -8,3 +10,25 @@ def gendiff():
     parser.add_argument('second_file')
     parser.add_argument('-f', "--format", help='set format of output')
     return parser.parse_args()
+
+
+def generate_diff(first_file, second_file):
+    file_1 = json.load(open(first_file))
+    file_2 = json.load(open(second_file))
+    file_1 = dict(sorted(file_1.items()))
+    file_2 = dict(sorted(file_2.items()))
+    result = '{\n'
+    for key in file_1:
+        if key in file_2:
+            if file_1[key] == file_2[key]:
+                result += f'  {key}: {file_1[key]}\n'
+            else:
+                result += f'- {key}: {file_1[key]}\n'
+                result += f'+ {key}: {file_2[key]}\n'
+        else:
+            result += f'- {key}: {file_1[key]}\n'
+    for key in file_2:
+        if key not in file_1:
+            result += f'+ {key}: {file_2[key]}\n'
+    result += '}'
+    return result
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,9 +1,10 @@
-from gendiff.cli import gendiff
+from gendiff.cli import gendiff, generate_diff
 
 
 def main():
     args = gendiff()
-    print(args.first_file, args.second_file)
+    print(
+        generate_diff(args.first_file, args.second_file))
 
 
 if __name__ == '__main__':
