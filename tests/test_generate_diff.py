e618f518102aded2d552571df081d5781d2398bc
dmitry_rodin <dimoon-96@yandex.ru>
2024-03-14 14:10:35 +0700

    add tests yaml file

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -8,9 +8,17 @@ def get_path(file_name):
     return current_dir / 'fixtures' / file_name
 
 
-def test_generate_diff():
+def test_generate_diff_json():
     input_1 = get_path('file_1.json')
     input_2 = get_path('file_2.json')
+    result = get_path('result.txt')
+    with open(result) as result:
+        assert generate_diff(input_1, input_2) == result.read()
+
+
+def test_generate_diff_yaml():
+    input_1 = get_path('file_1.yaml')
+    input_2 = get_path('file_2.yml')
     result = get_path('result.txt')
     with open(result) as result:
         assert generate_diff(input_1, input_2) == result.read()
