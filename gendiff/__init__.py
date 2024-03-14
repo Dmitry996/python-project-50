5645ba7f2c5130be7a31ba986f45a531d2de1669
dmitry_rodin <dimoon-96@yandex.ru>
2024-03-14 08:44:15 +0700

    add parser

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,6 +1,9 @@
 from .generate_diff import generate_diff
+from .parser import parser
+
 
 __all__ = (
     'generate_diff',
+    'parser',
 
 )
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
-import argparse
 from gendiff import generate_diff
-
-
-parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-parser.add_argument('first_file')
-parser.add_argument('second_file')
-parser.add_argument('-f', '--format', help='set format of output')
-
-args = parser.parse_args()
-first_file = args.first_file
-second_file = args.second_file
+from gendiff import parser
 
 
 def main():
+    first_file, second_file = parser()
     generate_diff(first_file, second_file)
 
 
