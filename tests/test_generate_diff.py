8cc075c42fc140c9cc9edd7ddc94adc6aabe87ca
dmitry_rodin <dimoon-96@yandex.ru>
2024-03-14 10:11:04 +0700

    add test_generate diff

---
+++ tests/test_generate_diff.py
@@ -0,0 +1,16 @@
+from gendiff import generate_diff
+from pathlib import Path
+
+
+def get_path(file_name):
+    p = Path(__file__)
+    current_dir = p.absolute().parent
+    return current_dir / 'fixtures' / file_name
+
+
+def test_generate_diff():
+    input_1 = get_path('file_1.json')
+    input_2 = get_path('file_2.json')
+    result = get_path('result.txt')
+    with open(result) as result:
+        assert generate_diff(input_1, input_2) == result.read()
