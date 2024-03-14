ed615e0e3a088f3f269d277e180eab23f5471a1c
dmitry_rodin <dimoon-96@yandex.ru>
2024-03-14 21:09:32 +0700

    fixed tests file

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -9,16 +9,16 @@ def get_path(file_name):
 
 
 def test_generate_diff_json():
-    input_1 = get_path('file_1.json')
-    input_2 = get_path('file_2.json')
-    result = get_path('result.txt')
+    input_1 = get_path('file_1_plain.json')
+    input_2 = get_path('file_2_plain.json')
+    result = get_path('result_plain.txt')
     with open(result) as result:
         assert generate_diff(input_1, input_2) == result.read()
 
 
 def test_generate_diff_yaml():
-    input_1 = get_path('file_1.yaml')
-    input_2 = get_path('file_2.yml')
+    input_1 = get_path('file_1_plain.yaml')
+    input_2 = get_path('file_2_plain.yml')
     result = get_path('result.txt')
     with open(result) as result:
         assert generate_diff(input_1, input_2) == result.read()
