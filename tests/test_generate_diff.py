fd9e611b88e714e618c66c3053c81452a90cdcbb
dmitry_rodin <dimoon-96@yandex.ru>
2024-03-18 10:51:03 +0700

    add tests for plain

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -11,13 +11,18 @@ def get_path(file_name):
 
 test_files = [
     ('file_1.json', 'file_2.json', 'result_stylish.txt', 'stylish'),
-    ('file_1.yaml', 'file_2.yml', 'result_stylish.txt', 'stylish')]
+    ('file_1.yaml', 'file_2.yml', 'result_stylish.txt', 'stylish'),
+    ('file_1.yaml', 'file_2.json', 'result_stylish.txt', 'stylish'),
+    ('file_1.json', 'file_2.json', 'result_plain.txt', 'plain'),
+    ('file_1.yaml', 'file_2.yml', 'result_plain.txt', 'plain'),
+    ('file_1.yaml', 'file_2.json', 'result_plain.txt', 'plain')
+]
 
 
-@pytest.mark.parametrize('file_1, file_2, result_file, format', test_files)
-def test_generate_diff(file_1, file_2, result_file, format):
+@pytest.mark.parametrize('file_1, file_2, result_file, formatter', test_files)
+def test_generate_diff(file_1, file_2, result_file, formatter):
     input_1 = get_path(file_1)
     input_2 = get_path(file_2)
     result = get_path(result_file)
     with open(result) as res:
-        assert generate_diff(input_1, input_2) == res.read()
+        assert generate_diff(input_1, input_2, formatter) == res.read()
